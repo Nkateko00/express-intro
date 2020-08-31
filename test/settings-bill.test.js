bb2a060d6adb5d23beac659855bc42d78f7800a7
Nkateko00 <tekonku@icloud.com>
2020-08-31 15:43:54 +0200

    error handling-travis

--- test/settings-bill.test.js
+++ test/settings-bill.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-const SettingsBill = require('../test/setting-bill');
+const SettingsBill = require('../test/settings-bill');
 
 describe('settings-bill', function(){
 
